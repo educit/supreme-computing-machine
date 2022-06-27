af5d4beba3bfc3bac5d7f649d71d95b161bf65fa
educit <kenneth.jacob@gmail.com>
2022-06-27 16:46:59 +0800

    Update DanceTeamInit.cs
    Assign 5 dancers per team

--- GAM404_Brief_01_StartingPoint/Assets/Scripts/DanceTeamInit.cs
+++ GAM404_Brief_01_StartingPoint/Assets/Scripts/DanceTeamInit.cs
@@ -12,7 +12,7 @@ public class DanceTeamInit : MonoBehaviour
     public DanceTeam teamA, teamB; // A reference to our teamA and teamB DanceTeam instances.
 
     public GameObject dancerPrefab; // This is the dancer that gets spawned in for each team.
-    public int dancersPerSide = 3; // This is the number of dancers for each team, if you want more, you need to modify this in the inspector.
+    public int dancersPerSide = 5; // This is the number of dancers for each team, if you want more, you need to modify this in the inspector.
     public CharacterNameGenerator nameGenerator; // This is a reference to our CharacterNameGenerator instance.
     private CharacterName[] teamACharacterNames; // An array to hold all our character names of TeamA.
     private CharacterName[] teamBcharacterNames; // An array to hold all the character names of TeamB
